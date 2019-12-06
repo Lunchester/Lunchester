98733d47b594830b54a3c86fde18331d821dffdf
Vincelicious <hello@vincentvandeursen.com>
2019-12-06 21:37:27 +0100

    added items to grocery list view

---
+++ src/views/GroceryList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+    <Item
+      title="Boeren tarwe tijger"
+      body="€2,60"
+      image="/img/boeren-tarwe-tijger-half.png"
+      quantityControls="true"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Item from "@/components/shared/BaseItemComponent.vue";
+
+export default Vue.extend({
+  components: {
+    Item
+  }
+});
+</script>
+
+<style scoped>
+.product:nth-child(even) {
+  background: #f5f5f5;
+}
+</style>
