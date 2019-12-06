86638564f6150bc89b1ac2f7da52678d8e662174
Vincelicious <hello@vincentvandeursen.com>
2019-12-06 23:04:03 +0100

    added header and search bar to stock list view

--- src/views/StockList.vue
+++ src/views/StockList.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <Header title="Stock" />
+    <Search />
     <Item
       title="Boeren tarwe tijger"
       body="Added on 20.11"
@@ -60,16 +62,20 @@
 <script lang="ts">
 import Vue from "vue";
 import Item from "@/components/shared/BaseItemComponent.vue";
+import Search from "@/components/shared/BaseSearchComponent.vue";
+import Header from "@/components/shared/BaseHeaderComponent.vue";
 
 export default Vue.extend({
   components: {
-    Item
+    Item,
+    Search,
+    Header
   }
 });
 </script>
 
 <style scoped>
-.product:nth-child(even) {
+.product:nth-child(odd) {
   background: #f5f5f5;
 }
 </style>
