5fe3d7eb42c77bc640f92dc58df5d06cbd88b585
Vincelicious <hello@vincentvandeursen.com>
2019-12-06 21:49:31 +0100

    added header to grocery list view

--- src/views/GroceryList.vue
+++ src/views/GroceryList.vue
@@ -1,59 +1,64 @@
 <template>
   <div>
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
-    <Item
-      title="Boeren tarwe tijger"
-      body="€2,60"
-      image="/img/boeren-tarwe-tijger-half.png"
-      quantityControls="true"
-    />
+    <div class="header">
+      <h1>Grocery List</h1>
+    </div>
+    <div class="products">
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+      <Item
+        title="Boeren tarwe tijger"
+        body="€2,60"
+        image="/img/boeren-tarwe-tijger-half.png"
+        quantityControls="true"
+      />
+    </div>
   </div>
 </template>
 
@@ -69,7 +74,15 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-.product:nth-child(even) {
+.header {
+  text-align: center;
+  color: #fff;
+  font-size: 18px;
+  padding: 24px 0;
+  background: #7ecf95;
+}
+
+.product:nth-child(odd) {
   background: #f5f5f5;
 }
 </style>
