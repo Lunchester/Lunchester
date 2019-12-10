5cc94e30d297c6ea5efc9e04e2dc84710f492c17
Vincelicious <hello@vincentvandeursen.com>
2019-12-10 21:35:54 +0100

    added header to special day view

---
+++ src/views/SpecialDay.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header title="Special Day" />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/shared/BaseHeaderComponent";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
