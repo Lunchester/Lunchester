02107e443ad1894e905f557885391a89119bb522
Vincelicious <hello@vincentvandeursen.com>
2020-01-02 15:21:18 +0100

    Added GIF to scanner view

---
+++ src/views/Scanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header title="Scanner" />
+    <img
+      style="width: 100vw; border-radius:24px;  bottom: -711px; position: absolute;  "
+      src="@/assets/scanner.gif"
+      alt=""
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { mapState } from "vuex";
+import Header from "@/components/shared/BaseHeaderComponent.vue";
+
+export default Vue.extend({
+  components: {
+    Header
+  }
+});
+</script>
